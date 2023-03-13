f72b118d8e5c9d2914c3da741d4e0c03cf5a30ae
Krishna Sharma <Ks9192174414@gmail.com>
2023-03-13 20:37:08 +0530

    Navbar Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <>
-      <div>
-        Profile
+    <div className="nav-side">
+      <div className="profile">
+        <div>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkDwTLbK9NwlI2rpMD2waNlXgGTb5-1ZV9Pu2wPv8jqyA1xaC0H6aElSl9iP_n_LLLtnI&usqp=CAU" />
+          <p className="name">Krishna Sharma</p>
+          <div className="title">Software Engineer</div>
+          <div className="title">Full stack developer</div>
+        </div>
+        </div>
+      <div className="nav-items">
+        <Link to="/">About</Link>
+        <Link to="/experience">Experience</Link>
+        <Link to="/education">Education</Link>
+        <Link to="/skills">Skills</Link>
+        <Link to="/interest">Interest</Link>
       </div>
-      <div className='nav-items'>
-        <Link to='/'>About</Link>
-        <Link to='/experience'>Experience</Link>
-        <Link to='/education'>Education</Link>
-        <Link to='/skills'>Skills</Link>
-        <Link to='/interest'>Interest</Link>
-      </div>
-    </>
+    </div>
   );
 };
 
