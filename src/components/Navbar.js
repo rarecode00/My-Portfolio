29f40a47ab2f024de755e6c03bf7f200e8c48312
Krishna Sharma <Ks9192174414@gmail.com>
2023-03-14 15:22:58 +0530

    about section is completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React , {useState , useEffect} from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import firebase from './firebase'
 
 const Navbar = () => {
   const [imageURL , setImageURL] = useState('')
+  const location = useLocation()
   useEffect(() => {
     const storageRef = firebase.storage().ref();
     var listRef = storageRef.child('profile/')
@@ -27,6 +28,7 @@ const Navbar = () => {
 
   return (
     <div className="nav-side">
+      {console.log(location.pathname)}
       <div className="profile">
         <div>
           <img src= {imageURL}/>
@@ -36,11 +38,11 @@ const Navbar = () => {
         </div>
         </div>
       <div className="nav-items">
-        <Link to="/" className="border">About</Link>
-        <Link to="/experience">Experience</Link>
-        <Link to="/education">Education</Link>
-        <Link to="/skills">Skills</Link>
-        <Link to="/interest">Interest</Link>
+        <Link to="/" className={location.pathname === '/' ? 'border' : 'br-none'}>About</Link>
+        <Link to="/experience" className={location.pathname === '/experience' ? 'border' : 'br-none'}>Experience</Link>
+        <Link to="/education" className={location.pathname === '/education' ? 'border' : 'br-none'}>Education</Link>
+        <Link to="/skills" className={location.pathname === '/skills' ? 'border' : 'br-none'}>Skills</Link>
+        <Link to="/interest" className={location.pathname === '/interest' ? 'border' : 'br-none'}>Interest</Link>
       </div>
     </div>
   );
